737272f63e29af2095e763604039091ba49a24da
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-29 12:20:52 -0600

    Feat: Routes and controller method for get one item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+
   def index
     render json:(ItemSerializer.new(Item.all))
   end
+
+  def show
+    render json: ItemSerializer.new(@item)
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
 
 end
