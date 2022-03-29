2b6c6c417b71c8aeedb9de1327e3cb38f71596e7
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-29 08:00:46 -0600

    Feat: Add MerchantItemsController

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::MerchantItemsController < ApplicationController
+
+  def index
+    if params[:item_id]
+     merchant_id = Item.find(params[:item_id]).merchant_id
+     merchant = Merchant.find(merchant_id)
+     render json: MerchantSerializer.new(merchant)
+   else
+     merchant = Merchant.find(params[:merchant_id])
+     render json: ItemSerializer.new(merchant.items)
+   end
+  end
+end
