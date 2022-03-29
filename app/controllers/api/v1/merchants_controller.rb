25ab2dc606dde207d9c8c0f271019d5d6fb4d85b
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-28 19:25:07 -0600

    Feat: Add show to Merchants Controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::MerchantsController < ApplicationController
+  before_action :set_merchant, only: [:show]
 
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(@merchant)
+  end
+
+  private
+    def set_merchant
+      @merchant = Merchant.find(params[:id])
+    end
 
 end
