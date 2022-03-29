44ee1ebab35858b4d387177b86bc94260e90230c
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-29 11:53:25 -0600

    Feat: Add route for index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, controller: 'merchant_items', only: [:index]
       end
+      resources :items, only: [:index]
     end
   end
 end
