ae2ec671008e3a44ec876550e05efe5b6c2116b2
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-30 19:42:20 -0600

    Test:  Happy Path Spec for "Find One Merchant by Name Fragment"

---
+++ spec/requests/api/vi/merchants/merchant_search_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Search endpoint' do
+  context 'happy path' do
+    it 'returns one merchant' do
+      merchant_1 = create(:merchant, name: 'Zena')
+      merchant_2 = create(:merchant, name: 'Abba')
+
+      get '/api/v1/merchants/find?name=Zena'
+
+      results = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(200)
+
+      expect(results).to have_key(:data)
+      expect(results[:data].count).to eq(3)
+
+      results_data = results[:data]
+
+      expect(results_data).to have_key(:id)
+      expect(results_data[:id]).to be_a(String)
+
+      expect(results_data).to have_key(:type)
+      expect(results_data[:type]).to eq("merchant")
+
+      expect(results_data).to have_key(:attributes)
+      expect(results_data[:attributes]).to be_a(Hash)
+
+      merchant = results_data[:attributes]
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to eq("Zena")
+    
+
+    end
+  end
+  context 'sad path/ edge case' do
+  end
+end
