0837972317b313cf2bbcdebad600911cab941287
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-30 19:59:11 -0600

    Refactor: Rubocop "Find One Merchant by Name Fragment" Happy path

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::SearchesController < ApplicationController
-
   def find_merchant
-    if params[:name] == nil
-      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} }, status: :bad_request
-    elsif params[:name] == ""
-      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} }, status: :bad_request
-    elsif params[:name].class == String && params[:name].length > 0
+    if params[:name].nil?
+      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} },
+             status: :bad_request
+    elsif params[:name] == ''
+      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} },
+             status: :bad_request
+    elsif params[:name].instance_of?(String) && params[:name].length > 0
       @merchant = Merchant.search(params[:name]).first
     end
     render json: MerchantSerializer.new(@merchant)
