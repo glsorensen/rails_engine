303b465a962a378a5925cdb7cc23540aeb8d86bf
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-30 19:43:25 -0600

    Feat: Controller for "Find One Merchant by Name Fragment"

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SearchesController < ApplicationController
+
+  def find_merchant
+    if params[:name] == nil
+      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} }, status: :bad_request
+    elsif params[:name] == ""
+      render json: { status: 'BAD REQUEST', message: 'search parameters cannot be empty', data: {} }, status: :bad_request
+    elsif params[:name].class == String && params[:name].length > 0
+      @merchant = Merchant.search(params[:name]).first
+    end
+    render json: MerchantSerializer.new(@merchant)
+  end
+end
