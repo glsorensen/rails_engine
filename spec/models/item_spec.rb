6be8b020c61a7700f9fe2e314fa476b9ec58e64e
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-01 08:39:17 -0600

    Test: Model test for .get_empty_invoices

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,35 @@ RSpec.describe Item do
     it { should validate_presence_of(:name) }
     it { should validate_numericality_of(:unit_price) }
   end
+  describe '.instance methods' do
+    context '.find_empty_invoices' do
+        it 'gets invoices where this item is the only item on it' do
+        merchant = Merchant.create!(name: "Jim")
+        customer1 = Customer.create(first_name: "Gunnar", last_name: 'Sorensen')
+        customer2 = Customer.create(first_name: "Priska", last_name: 'Sorensen')
+        customer3 = Customer.create(first_name: "Lynn", last_name: 'Sorensen')
+        invoice1 = Invoice.create!(customer_id: customer1.id, merchant_id: merchant.id )
+        invoice2 = Invoice.create!(customer_id: customer2.id, merchant_id: merchant.id )
+        invoice3 = Invoice.create!(customer_id: customer3.id, merchant_id: merchant.id )
+        item1 = Item.create!(name: 'Default Item 1',
+                description: 'describes things about the item',
+                unit_price: 10.5,
+                merchant_id: merchant.id)
+        item2 = Item.create!(name: 'Default Item 2',
+                description: 'describes things about the item',
+                unit_price: 10.5,
+                merchant_id: merchant.id)
+        invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice2.id)
+        invoice_item3 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice3.id)
+        invoice_item4 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice3.id)
+
+
+        expect(item1.find_empty_invoices).to include(item1.id)
+        expect(item1.find_empty_invoices).to include(item2.id)
+      end
+    end
+  end
 
   describe 'class methods' do
     describe '::search' do
